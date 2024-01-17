d168e62d97bd53215fe7642f863770542da86ce7
Felipe Cavalcanti <fb.cavalcanti1@gmail.com>
2024-01-17 20:59:15 -0300

    OrderStatus Enum

--- src/main/java/pna1/com/example/pna1/entities/Order.java
+++ src/main/java/pna1/com/example/pna1/entities/Order.java
@@ -4,6 +4,7 @@ package pna1.com.example.pna1.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import pna1.com.example.pna1.entities.enums.OrderStatus;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -22,6 +23,7 @@ public class Order implements Serializable {
     @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -29,10 +31,11 @@ public class Order implements Serializable {
     public Order() {
 
     }
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -49,6 +52,14 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
